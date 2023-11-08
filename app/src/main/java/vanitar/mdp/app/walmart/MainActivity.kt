16609f34e10bf3259ba48b3c64692db0c6bd3545
vanitarheavens <vanitarheavens@gmail.com>
2023-11-07 21:46:48 -0600

    initial commit

---
+++ app/src/main/java/vanitar/mdp/app/walmart/MainActivity.kt
@@ -0,0 +1,16 @@
+package vanitar.mdp.app.walmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import henry.mdp.app.walmart.R
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
